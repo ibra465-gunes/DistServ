a9a38ee178eb1ac785dcde8c37634f2d7383813a
ibra465-gunes <79796590+ibra465-gunes@users.noreply.github.com>
2025-11-14 19:57:20 +0300

    v1.0: Version.java eklendi – sürüm bilgisi tanımlandı

---
+++ src/util/Version.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Version {
+    public static final String CURRENT = "v1.0";
+    public static final String RELEASE_DATE = "2025-11-14";
+    public static final String DESCRIPTION = "Modüler mimari, enum geçişi, switch-case, thread senkronizasyonu, loglama, v0.1 düzeltmeleri";
+}
